250057b23e72daecb4050ee4f11941ccfb14b42a
TB831 <druu831@gmail.com>
2019-10-22 11:36:17 -0700

    basic template added

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-print(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def helloWorld():
+  return render_template('index.html')
+
+@app.route('/blog')
+def blog():
+  return 'this is my blog'
+
+@app.route('/blog/2020/dogs')
+def blog2():
+  return 'this is my dog'
