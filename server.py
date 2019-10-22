3aeca6656b003d53ecb281760ac49a2547dbec94
TB831 <druu831@gmail.com>
2019-10-22 16:53:02 -0700

    Links fixed. work.html updated. form submission route added

--- server.py
+++ server.py
@@ -13,6 +13,10 @@ def about():
 def works():
   return render_template('works.html')
 
+@app.route('/work')
+def work():
+  return render_template('work.html')  
+
 @app.route('/contact')
 def contact():
   return render_template('contact.html') 
@@ -21,6 +25,14 @@ def contact():
 def components():
   return render_template('components.html')
 
+@app.route('/<string:pageName>')
+def htmlPage(pageName):
+  return render_template(pageName)
+
 @app.route('/blog')
 def blog():
   return 'My Blog!'
+
+@app.route('/submitForm', methods=['POST', 'GET'])
+def submitForm():
+  return 'form submitted hoooorayyy'
